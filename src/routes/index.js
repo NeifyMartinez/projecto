78d9bc4883af0aeed64bb7a0df291d0fe1576bd2
NeifyMartinez <njmartinezinc@gmail.com>
2022-07-08 19:04:25 -0700

    title router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,19 +3,19 @@ import { Router } from 'express';
 const router = Router();
 //routes
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Homepage Neify Martinez' });
 });
 
 router.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About Me' });
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', { title: 'Contact Me' });
 });
 
 router.get('/projects', (req, res) => {
-    res.render('projects');
+    res.render('projects', { title: 'Projects Page' });
 });
 
 export default router
